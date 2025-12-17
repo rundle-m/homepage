a908ca6cc188bc0bb32da5fa5a0b89b108d38cfd
rundle-m <rundlemmall@gmail.com>
2025-12-17 21:27:39 +1100

    add farcaster redirect

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/farcaster.json',
+        destination: 'https://api.farcaster.xyz/miniapps/hosted-manifest/019b2bd5-f590-31a4-0a6d-237e9a361e93', // <--- PASTE YOUR LINK HERE
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
